3e9dde6232514c8317a6eb7cc28a65758a1cb1f7
MaoBA1 <94516860+MaoBA1@users.noreply.github.com>
2022-04-25 18:26:05 +0300

    123

---
+++ src/Utilis/AppColors.js
@@ -0,0 +1,10 @@
+export default {
+    red1: '#e63946',
+    red2: '#ef233c',
+    pinkRed: '#f28482',
+    pink: '#f5cac3',
+    blackBlue: '#2b2d42',
+    blue1: '#0077b6',
+    blue2:'#48cae4',
+
+}
--- src/screens/stores/index.js
+++ src/screens/stores/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, ActivityIndicator, FlatList } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../../store/actions';
+import Colors from '../../Utilis/AppColors';
+
+
 
 const StoresScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const dispatch = useDispatch();
+
+    const getStores = useCallback(async() => {
+        setIsLoading(true);
+        let action = actions.get_store_action();
+        try{
+            await dispatch(action);
+            setIsLoading(false);
+        } catch(err) {
+            setIsLoading(false)
+        }
+    },[setIsLoading,actions.get_store_action, dispatch]);
+
+    useEffect(() => {
+        getStores();
+    },[getStores]);
+
+    const allStores = useSelector(state => state.allStores);
+    let appStores = allStores.allStores.Stores;       
+    
     return(
         <View>
-            <Text>Stores Screen</Text>
+            {
+                isLoading ?
+                (
+                    <ActivityIndicator
+                        color={Colors.red1}
+                        size='large'
+                    />
+                )
+                :
+                (
+                    <FlatList
+                        data={appStores}
+                        keyExtractor={item => item._id}
+                        renderItem={store => <Text style={{fontSize:50}}>{store.item.storeName}</Text>}
+                    />
+                )
+            }
         </View>
     )
 }
